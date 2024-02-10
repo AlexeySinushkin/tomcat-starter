3b2bc3ff4c22ae7585bce78ff3646af678bac0c6
AlexeySInushkin <a.sinushkin@id-mt.ru>
2024-02-10 16:54:25 +0800

    Update config.rs

--- src-tauri/src/domain/config.rs
+++ src-tauri/src/domain/config.rs
@@ -1,7 +1,9 @@
-use serde::{Deserialize, Serialize};
-use super::property::Property;
-use super::tasks::{CopyWarToRandomDir,  RunTomcat, TaskType};
+use std::borrow::Borrow;
+use std::collections::{BTreeMap, HashMap};
 
+use super::property::Property;
+use super::tasks::{CopyWarToRandomDir, RunTomcat, TaskType};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Variables {
@@ -14,7 +16,7 @@ pub struct Variables {
 
 pub struct ServerRun {
     pub server_run_name: String,
-    pub server_name: String,    //reference to Server
+    pub server_name: String, //reference to Server
     pub tasks: Vec<TaskType>,
 }
 
@@ -33,11 +35,11 @@ pub struct ConfigurationDto {
 pub struct ServerRunDto {
     #[serde(rename = "serverRunName")]
     pub server_run_name: String,
-    
+
     #[serde(rename = "serverName")]
     pub server_name: String,
-    
-    pub order:Vec<String>,
+
+    pub order: Vec<String>,
 
     #[serde(rename = "warToRandom")]
     pub war_to_random: Vec<CopyWarToRandomDirDto>,
@@ -45,8 +47,6 @@ pub struct ServerRunDto {
     #[serde(rename = "runTomcat")]
     pub run_tomcat: Vec<RunTomcatDto>,
 }
-
-
 
 #[derive(Deserialize, Serialize, Debug)]
 pub enum TaskTypeDto {
@@ -56,35 +56,121 @@ pub enum TaskTypeDto {
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct CopyWarToRandomDirDto {
-    #[serde(rename = "type")]
-    pub task_type: TaskTypeDto,
     pub id: String,
     #[serde(rename = "sourceWarPath")]
     pub source_war_path: String,
     #[serde(rename = "destinationCatalinaBase")]
     pub destination_catalina_base: String,
     #[serde(rename = "destinationWarName")]
-    pub destination_war_name: String
+    pub destination_war_name: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct RunTomcatDto {
-    #[serde(rename = "type")]
-    pub task_type: TaskTypeDto,
     pub id: String,
     #[serde(rename = "catalinaOpts")]
     pub catalina_opts: String,
     #[serde(rename = "listenPort")]
     pub listen_port: String,
     #[serde(rename = "jdpaPort")]
-    pub jdpa_port: String
+    pub jdpa_port: String,
 }
 
+pub fn convert_to_dto(config: Configuration) -> ConfigurationDto {
+    let mut runs: Vec<ServerRunDto> = vec![];
 
-pub fn convertToDto(config: Configuration) {
-   /* let runs : Vec<ServerRunDto> = vec![];
-    for item in config.runs.iter() {
-
+    for run in config.runs.into_iter() {
+        let mut war_to_random: Vec<CopyWarToRandomDirDto> = vec![];
+        let mut run_tomcat: Vec<RunTomcatDto> = vec![];
+        let mut order: Vec<String> = vec![];
+        for task in run.tasks.into_iter() {
+            if let TaskType::CopyWarRandomType(task) = task {
+                order.push(task.id.clone());
+                war_to_random.push(map_copy_war_to_dto(task));
+            } else if let TaskType::RunTomcatType(task) = task {
+                order.push(task.id.clone());
+                run_tomcat.push(map_run_tomcat_to_dto(task));
+            }
+        }
+        runs.push(ServerRunDto {
+            server_name: run.server_name,
+            server_run_name: run.server_run_name,
+            war_to_random,
+            run_tomcat,
+            order,
+        })
     }
-    */
+    return ConfigurationDto {
+        vars: config.vars,
+        runs,
+    };
+}
+
+pub fn convert_from_dto(config: ConfigurationDto) -> Configuration {
+    let mut runs: Vec<ServerRun> = vec![];
+    for run in config.runs.into_iter() {
+        //let mut tasks : Vec<TaskType> = vec![];
+        let mut tasksMap = BTreeMap::new();
+
+        for task in run.war_to_random.into_iter() {
+            let id = task.id.clone();
+            let task = TaskType::CopyWarRandomType(map_copy_war_from_dto(task));
+            tasksMap.insert(id, task);
+        }
+        for task in run.run_tomcat.into_iter() {
+            let id = task.id.clone();
+            let task = TaskType::RunTomcatType(map_run_tomcat_from_dto(task));
+            tasksMap.insert(id, task);
+        }
+        let mut tasksOrdered: Vec<TaskType> = vec![];
+        for taskId in run.order.iter() {
+            let task = tasksMap.remove(taskId).unwrap();
+            tasksOrdered.push(task);
+        }
+        runs.push(ServerRun {
+            server_name: run.server_name,
+            server_run_name: run.server_run_name,
+            tasks: tasksOrdered,
+        })
+    }
+    return Configuration {
+        vars: config.vars,
+        runs,
+    };
+}
+
+fn map_copy_war_to_dto(task: CopyWarToRandomDir) -> CopyWarToRandomDirDto {
+    CopyWarToRandomDirDto {
+        id: task.id,
+        destination_catalina_base: task.destination_catalina_base,
+        destination_war_name: task.destination_war_name,
+        source_war_path: task.source_war_path,
+    }
+}
+
+fn map_run_tomcat_to_dto(task: RunTomcat) -> RunTomcatDto {
+    RunTomcatDto {
+        id: task.id,
+        catalina_opts: task.catalina_opts,
+        listen_port: task.listen_port,
+        jdpa_port: task.jdpa_port,
+    }
+}
+
+fn map_copy_war_from_dto(task: CopyWarToRandomDirDto) -> CopyWarToRandomDir {
+    CopyWarToRandomDir {
+        id: task.id,
+        destination_catalina_base: task.destination_catalina_base,
+        destination_war_name: task.destination_war_name,
+        source_war_path: task.source_war_path,
+    }
+}
+
+fn map_run_tomcat_from_dto(task: RunTomcatDto) -> RunTomcat {
+    RunTomcat {
+        id: task.id,
+        catalina_opts: task.catalina_opts,
+        listen_port: task.listen_port,
+        jdpa_port: task.jdpa_port,
+    }
 }
